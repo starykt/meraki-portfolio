ad453b8b754d41353a1584e442b04031efdd6098
gabrielamoreno <gabrielamoreno@jera.com.br>
2023-08-03 19:44:00 -0300

    [UPDATE] css for landing page

--- App/Views/home/index.php
+++ App/Views/home/index.php
@@ -1,10 +1,16 @@
+<link href="http://<?php echo APP_HOST; ?>/public/css/landing-page.css" rel="stylesheet">
 <body>
-<div class="container">
-    <h1>Meraki</h1>
-    <a href="http://<?= APP_HOST ?>/login/index">
-    <button class="button_play">
-    <i class="fas fa-play"></i>
-    </button>
-</a>
-</div>
+  <div class="container">
+    <div class="big-circle">
+      <div class="purple-play-button">
+      </div>
+      <div class="right-blue-label">
+      </div>
+    </div>
+  </div>
+
+  <div class="small-circle">
+    <div class="left-blue-label">
+    </div>
+  </div>
 </body>

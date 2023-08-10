6b3b204771367859153ff6f13a5ed09f09f5495d
gabrielamoreno <gabrielamoreno@jera.com.br>
2023-08-10 15:22:12 -0300

    [NEW] css for login page

--- App/Views/login/index.php
+++ App/Views/login/index.php
@@ -1,4 +1,5 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<link href="http://<?php echo APP_HOST; ?>/public/css/login-page.css" rel="stylesheet">
 <body>
 
 	<div class="loader-wrapper">
@@ -7,29 +8,31 @@
 		</span>
 	</div>
 
-	<div class="top-bar">
-	</div>
+	<div class="wrapper">
+		<div class="top-bar"></div>
 
-
-
-
-	<div class="login-form">
-		<h2 class="white-text">Sign In</h2>
-		<br>
-		<form  action="http://<?php echo APP_HOST; ?>/login/validation" method="POST">
-			<div class="input-container">
+		<div class="login-form">
+			<h2 class="sign-in">Login</h2>
+			<br>
+			<form action="http://<?php echo APP_HOST; ?>/login/validation" method="POST">
 				<input type="text" placeholder="Nickname#2728" name="email" required>
+				<div class="input-purple">
+					<input type="password" id="password" placeholder="Senha" name= "password" required>
+					<div class="eye-button" onclick="togglePasswordVisibility()">
+						<img src="">
+					</div>
+				</div>
+				<div class="forgot-password">Esqueceu a senha?</div>
+				<button class="btn-login" type="submit" class="">Login</button>
+			</form>
+			<div class="register-link">
+				<a href="http://<?= APP_HOST ?>/login/register">
+					<button class="not-a-player-button">Not a player?</button>
+				</a>
 			</div>
-			<div class="input-container">
-				<input type="password" id="password" placeholder="Password" name= "password" required>
-				<i class="toggle-password fas fa-eye" onclick="togglePasswordVisibility()"></i>
-			</div>
-			<button type="submit" class="purple-button">Login</button>
-		</form>
-		<div class="register-link">
-			<a href="http://<?= APP_HOST ?>/login/register">
-				<button class="not-a-player-button">Not a player?</button>
-			</a>
+		</div>
+		<div class="bottom-wave">
+			<img src="/public/images/wave.svg">
 		</div>
 	</div>
 	

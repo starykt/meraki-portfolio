8c518b058175dc9ace26ee70f21fb2395e08daed
Nicole <118059749+NisckzAlves@users.noreply.github.com>
2023-11-02 21:47:03 -0300

    AB#78

--- App/Controllers/ToolController.php
+++ App/Controllers/ToolController.php
@@ -24,84 +24,97 @@ class ToolController extends Controller
   }
 
 
-
   public function save()
   {
-      try {
-          if ($_FILES['icon']['error'] !== UPLOAD_ERR_OK) {
-              throw new Exception("Erro durante o upload do ícone.");
-          }
-  
-          $caption = $_POST['caption'];
-          if (empty($caption)) {
-              throw new Exception("O campo de legenda não pode estar vazio.");
-          }
-
-          $tool = new Tool();
-          $tool->setCaption($caption);
-  
-          $toolDAO = new ToolDAO();
-          $toolId = $toolDAO->save($tool);
-
-          $objUpload = new Upload($_FILES['icon']);
-          $objUpload->setName('icon-id' . $toolId);
-          $dir = 'public/images/tools';
-          $success = $objUpload->upload($dir);
-  
-          if (!$success) {
-              throw new Exception("Erro ao enviar o ícone da ferramenta.");
-          }
-
-          $tool->setIcon($objUpload->getBasename());
-          $toolDAO->updateIcon($toolId, $tool->getIcon());
-
-          Sessao::gravaMensagem("Ferramenta cadastrada com sucesso!");
-          $this->redirect('/tool');
-      } catch (Exception $e) {
-          Sessao::gravaErro("Erro ao cadastrar a ferramenta. " . $e->getMessage());
-          $this->redirect('/tool/register');
+    try {
+      if ($_FILES['icon']['error'] !== UPLOAD_ERR_OK) {
+        throw new Exception("Erro durante o upload do ícone.");
       }
-  }
-  
 
-  public function editar($params)
+      $caption = $_POST['caption'];
+      if (empty($caption)) {
+        throw new Exception("O campo de legenda não pode estar vazio.");
+      }
+
+      $tool = new Tool();
+      $tool->setCaption($caption);
+
+      $toolDAO = new ToolDAO();
+      $toolId = $toolDAO->save($tool);
+
+      $objUpload = new Upload($_FILES['icon']);
+      $objUpload->setName('icon-id' . $toolId);
+      $dir = 'public/images/tools';
+      $success = $objUpload->upload($dir);
+
+      if (!$success) {
+        throw new Exception("Erro ao enviar o ícone da ferramenta.");
+      }
+
+      $tool->setIcon($objUpload->getBasename());
+      $toolDAO->updateIcon($toolId, $tool->getIcon());
+
+      Sessao::gravaMensagem("Ferramenta cadastrada com sucesso!");
+      $this->redirect('/tool');
+    } catch (Exception $e) {
+      Sessao::gravaErro("Erro ao cadastrar a ferramenta. " . $e->getMessage());
+      $this->redirect('/tool/register');
+    }
+  }
+
+  public function alter($params)
   {
     $idTool = $params[0];
 
     $toolDao = new ToolDAO();
     $tool = $toolDao->getById($idTool);
-
-    if ($tool) {
-      $this->setViewParam('tool', $tool);
-      $this->render('tool/edit');
-    } else {
-      Sessao::gravaErro("Ferramenta não encontrada.");
-      $this->redirect('/tool');
-    }
+    $this->setViewParam('tool', $tool);
+    $this->render('/tool/alter');
   }
 
-  public function atualizar()
+  public function update()
   {
-    $idTool = $_POST['idTool'];
-    $icon = $_POST['icon'];
-    $caption = $_POST['caption'];
-
-    $tool = new Tool();
-    $tool->setIdTool($idTool);
-    $tool->setIcon($icon);
-    $tool->setCaption($caption);
-
-    $toolDao = new ToolDAO();
-
-    try {
-      $toolDao->edit($tool);
-      Sessao::gravaMensagem("Ferramenta atualizada com sucesso!");
-      $this->redirect('/tool');
-    } catch (Exception $e) {
-      Sessao::gravaErro("Erro ao atualizar a ferramenta. Por favor, tente novamente.");
-      $this->redirect('/tool/editar/' . $idTool);
-    }
+      $idTool = $_POST['idTool'];
+      $caption = $_POST['caption'];
+  
+      try {
+          if (empty($caption)) {
+              throw new Exception("O campo de legenda não pode estar vazio.");
+          }
+  
+          $toolDAO = new ToolDAO();
+          $tool = $toolDAO->getById($idTool);
+  
+          if (!$tool) {
+              throw new Exception("Ferramenta não encontrada.");
+          }
+  
+          $tool->setCaption($caption);
+  
+          if ($_FILES['icon']['error'] === UPLOAD_ERR_OK) {
+              $objUpload = new Upload($_FILES['icon']);
+              $objUpload->setName('icon-id' . $idTool);
+              $dir = 'public/images/tools';
+              $success = $objUpload->upload($dir);
+  
+              if (!$success) {
+                  throw new Exception("Erro ao enviar o novo ícone da ferramenta.");
+              }
+  
+              $tool->setIcon($objUpload->getBasename());
+              $toolDAO->updateIcon($idTool, $tool->getIcon());
+          }
+  
+          $toolDAO->edit($tool);
+          Sessao::gravaMensagem("Ferramenta atualizada com sucesso!");
+          $this->redirect('/tool');
+      } catch (Exception $e) {
+          Sessao::gravaErro("Erro ao atualizar a ferramenta. " . $e->getMessage());
+          $this->redirect('/tool/editar/' . $idTool);
+      }
   }
+  
+
 
   public function excluir($params)
   {
---
+++ App/Views/tool/alter.php
@@ -0,0 +1,18 @@
+<form action="http://<?= APP_HOST ?>/tool/update" method="POST" enctype="multipart/form-data">
+    <div>
+        <label for="icon">Ícone:</label>
+        <img src="http://<?php echo APP_HOST; ?>/public/images/tools/<?php echo $viewVar['tool']->getIcon(); ?>" width="200" height="200"> <br>
+        <input type="file" name="icon" id="icon" accept="image/*">
+    </div>
+
+    <div>
+        <label for="caption">Legenda</label>
+        <input type="text" name="caption" id="caption" value="<?php echo $viewVar['tool']->getCaption(); ?>" required>
+    </div>
+
+    <input type="hidden" name="idTool" value="<?php echo $viewVar['tool']->getIdTool(); ?>">
+
+    <div>
+        <button type="submit">Atualizar Ferramenta</button>
+    </div>
+</form>

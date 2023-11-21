5010d5ef4eda8d0c4267052ff50ec140d2f4575f
Gabriela Ferrara <starykt.gabs@gmail.com>
2023-11-21 15:27:45 -0300

    [NEW] responsive for login page

--- App/Views/login/index.php
+++ App/Views/login/index.php
@@ -20,22 +20,28 @@
      </div>
    <?php } ?>
 		<div class="login-form">
-			<h2 class="sign-in">Login</h2>
-			<br>
+			<div class="title-form-desktop">
+				<div class="register-link-desktop">
+					<a href="http://<?= APP_HOST ?>/login/register">
+						<button class="not-a-player-button-desktop">Not a player?</button>
+					</a>
+				</div>
+				<h2 class="sign-in">Sign In</h2>
+			</div>
 			<form action="http://<?php echo APP_HOST; ?>/login/validation" method="POST">
 				<input type="text" placeholder="Nickname#2728" name="email" required>
 				<div class="input-purple">
-					<input type="password" id="password" placeholder="Senha" name= "password" required>
+					<input type="password" id="password" placeholder="Password" name= "password" required>
 					<div class="eye-button" onclick="togglePasswordVisibility()">
 						<img src="">
 					</div>
 				</div>
-				<div class="forgot-password">Esqueceu a senha?</div>
+				<div class="forgot-password">Forgot password?</div>
 				<button class="btn-login" type="submit" class="">Login</button>
 			</form>
 			<div class="register-link">
 				<a href="http://<?= APP_HOST ?>/login/register">
-					<button class="not-a-player-button">Not a player?</button>
+					<button class="not-a-player-button-mobile">Not a player?</button>
 				</a>
 			</div>
 		</div>

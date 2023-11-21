6fa0e9b0424da37150efb4326c7238f05a171660
Gabriela Ferrara <starykt.gabs@gmail.com>
2023-11-21 18:31:26 -0300

    [NEW] responsive for new user

--- App/Views/login/register.php
+++ App/Views/login/register.php
@@ -3,38 +3,39 @@
   <div class="top-bar"></div>
   <div class="wrapper">
     <div class="signup-form">
-    <?php
-   if ($Sessao::retornaErro()) { ?>
-     <div class="alert alert-warning" role="alert">
-       <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-       <?php foreach ($Sessao::retornaErro() as $key => $mensagem) {
-         echo $mensagem . "<br />";
-       } ?>
-     </div>
-   <?php } ?>
+      <?php
+        if ($Sessao::retornaErro()) { ?>
+        <div class="alert alert-warning" role="alert">
+          <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+          <?php foreach ($Sessao::retornaErro() as $key => $mensagem) {
+            echo $mensagem . "<br />";
+          } ?>
+        </div>
+      <?php } ?>
       <form action="http://<?php echo APP_HOST; ?>/login/save" method="post">
-        <h2>Novo perfil</h2>
           <div>
             <input type="text" name="nickname" placeholder="Nickname" required>
           </div>
           <div>
-            <input type="email" name="email" placeholder="Email" required>
+            <input type="email" name="email" placeholder="E-mail" required>
           </div>
           <div>
-            <input type="password" name="password" placeholder="Senha" required>
+            <input type="password" name="password" placeholder="Password" required>
           </div>
           <div>
-            <input type="password" name="password_confirm" placeholder="Confirmação de senha" required>
+            <input type="password" name="password_confirm" placeholder="Confirm password" required>
           </div>
-          <button type="submit" class="continue-button">
-            CONTINUE
-            <img src="/public/images/playButton.png" />
-          </button>
+          <div class="button-position">
+            <button type="submit" class="continue-button">
+              CONTINUE
+              <img src="/public/images/playButton.png" />
+            </button>
+          </div>
         </form>
     </div>
-    <!-- <div class="gameControl">
-            <img src="/public/images/videoGameControl.svg" />
-          </div> -->
+    <div class="gameControl">
+      <img src="/public/images/videoGameControl.svg"></img>
+    </div>
     <div class="bottom-wave">
       <img src="/public/images/wave.svg">
     </div>
